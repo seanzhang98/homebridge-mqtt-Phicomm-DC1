69f11aa03b76153f6c9d64ae243c1967192365bf
Adrian QB <adrianquevedobenito@gmail.com>
2020-05-26 14:50:04 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,54 @@
+# Homebridge MQTT Power Strip
+
+ [![npm version](https://badge.fury.io/js/homebridge-mqtt-power-strip.svg)](https://badge.fury.io/js/https://www.npmjs.com/package/homebridge-mqtt-power-strip) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+
+A Homebridge plugin to create grouped switches to control a power strip through MQTT.
+
+## Installation
+
+Run `npm install homebridge-mqtt-power-strip -g`
+
+## Configuration
+
+**Note**: The `{id}` string in the topics is replaced by a number (starting by one) representing each relay. So for the first relay the `{id}` will be replaced by `1` and so on. The maximum number is `relayCount`.
+
+| Key | Subkey | Required | Default Value | Note |
+|-----|--------|----------|---------------|------|
+| accessory | | Yes | | Must be 'mqttPowerStrip'. |
+| name | | Yes | | The name of your power strip. |
+| manufacturer | | No | ***REMOVED*** | The manufacturer of the power strip. |
+| model | | No | MQTT Power Strip | The model of the power strip. |
+| serial | | No | 00N00 | The serial number of the power strip. |
+| relayCount | | Yes | | The amount of relays your power strip has (as int). |
+| mqtt | | Yes | | The MQTT configuration. |
+| | host | Yes | | The MQTT host. |
+| | port | Yes | | The MQTT port (as int). |
+| | username | No | | The username to log in. |
+| | password | No | | The password to log in. |
+| | qos | No | 1 | The Quality Of Service (subscribe and pusblish). |
+| | reconnectInterval | No | 2000 (ms) | The time between reconnections when the connection is lost with the broker (in ms). |
+| | connectTimeout | No | 10000 (ms) | The time to timeout with the broker (in ms). |
+| | maxErrorCount | No | 10 | Maximum time of reconnection attempts with the broker until the plugin gives up. You can use -1 to disable it. |
+| topics | | Yes (at least one topic is needed) | | An object with the topics. |
+| | setOn | No | | The topic to set a relay state. Use `{id}` in the topic for it to be replaced  with a number from 1 - relayCount (see [config. example](example-config.json)). |
+| | getOn | No |  | The topic to get a relay state. Use `{id}` in the topic for it to be replaced  with a number from 1 - relayCount (see [config. example](example-config.json)). |
+| | retrieve | No | | An object that contains the topics needed to retrieve the state of the relays at boot.
+| | notify (inside retrieve) | No | | A topic to sent a message to retrieve the data. You can use `{id}`. |
+| | get (inside retrieve) | No | | A topic to receive the state of each relay. You can use `{id}` and if it's the same topic as the getOn, you don't need to specify it. |
+| | message (inside retrieve) | No | | The message to be sent on the notify topic. If it is not specified an empty message is sent. |
+| names | | No | An array with `"{name} Switch {id}"` i.e. `Test power strip Switch 1` | An array that contains the name for each id IN ORDER, so the first element is the id=1 and so on. |
+| onValue | | No | `"true"` | The value the device sends as 'on'. |
+| offValue | | No | `"false"` | The value the device sends as 'off'. |
+
+## Roadmap
+
+- [ ] Add support for SSL on the MQTT connection.
+- [ ] Allow to specify custom ids instead of numbers from 1 - relayCount.
+- [ ] Log less errors when something occurs.
+
+## Bugs
+
+If you discover a bug, please open an issue and I will try to solve it as fast as I can. Please provide the logs (the console output) and configuration file (only the relevant part) and be careful to not provide the MQTT username and password (replace those values with other ones) when submitting the issue.
+
+## License
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,174 @@
+var mqtt = require('mqtt')
+
+var Service, Characteristic, HomebridgeAPI, UUIDGen;
+var that;
+
+module.exports = function(homebridge) {
+
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  HomebridgeAPI = homebridge;
+  UUIDGen = homebridge.hap.uuid;
+  homebridge.registerAccessory("homebridge-mqtt-power-strip", "mqttPowerStrip", PowerStrip);
+}
+
+function PowerStrip(log, config) {
+  this.log = log;
+  this.config = config;
+  this.name = config.name;
+  this.services = [];
+
+  if (!config) {
+    return;
+  }
+
+  if (!this.name) {
+    this.log("\x1b[31mName not specified!\x1b[0m");
+    return;
+  } else if (!config.relayCount) {
+    this.log("\x1b[31mRelay count not specified!\x1b[0m");
+    return;
+  } else if (!config.mqtt) {
+    this.log("\x1b[31mMQTT config not specified!\x1b[0m");
+    return;
+  } else if (!config.mqtt.host || !config.mqtt.port) {
+    this.log("\x1b[31mYou need to specify the MQTT host and port!\x1b[0m");
+    return;
+  } else if (!config.topics || (!config.topics.getOn && !config.topics.setOn)) {
+    this.log("\x1b[31mYou need to specify the topics!\x1b[0m");
+    return;
+  }
+
+   // TODO Allow SSL.
+  var mqttOptions = {
+    port: config.mqtt.port,
+    clientId: 'homebridge_' + this.name,
+    username: config.mqtt.username,
+    password: config.mqtt.password,
+    reconnectPeriod: config.mqtt.reconnectInterval || 2000,
+    connectTimeout: config.mqtt.connectTimeout || 10000
+  }
+
+  this.mqttClient = mqtt.connect('mqtt://' + config.mqtt.host, mqttOptions);
+  this.mqttClient.on('message', this.mqttMessage);
+  this.mqttClient.on('connect', this.mqttConnect);
+  this.mqttClient.on('error', this.mqttError);
+
+  this.mqttErrorCount = 0;
+  this.maxMqttErrorCount = config.mqtt.maxErrorCount || 10;
+
+  var informationService = new Service.AccessoryInformation();
+
+  informationService
+    .setCharacteristic(Characteristic.Manufacturer, config.manufacturer || "Oxixes")
+    .setCharacteristic(Characteristic.Model, config.model || "MQTT Power Strip")
+    .setCharacteristic(Characteristic.SerialNumber, config.serial || "00N00")
+    .setCharacteristic(Characteristic.FirmwareRevision, "1.0.0");
+
+  for (let i = 0; i < config.relayCount; i++) {
+    let uuid = UUIDGen.generate(this.name + i);
+    let name;
+
+    if (this.config.names && this.config.names[i]) {
+      name = this.config.names[i];
+    } else {
+      name = this.name + " Switch " + (i + 1);
+    }
+
+    let switchService = new Service.Outlet(name, uuid);
+    switchService.getCharacteristic(Characteristic.On)
+      .on('set', this.setOn.bind(this, i + 1));
+
+    this.services.push(switchService);
+  }
+
+  this.services.push(informationService);
+
+  that = this;
+}
+
+PowerStrip.prototype.mqttConnect = function() {
+  that.log("Successful conection to " + that.config.mqtt.host + ":" + that.config.mqtt.port);
+
+  that.mqttGetOnTopics = [];
+
+  if(that.config.topics.getOn) {
+    for (i = 0; i < that.config.relayCount; i++) {
+      let topic = that.config.topics.getOn.replace(/{id}/, i + 1);
+      that.mqttClient.subscribe(topic, {'qos': that.config.mqtt.qos || 1});
+      that.mqttGetOnTopics.push([topic, i]);
+    }
+  }
+
+  if (that.config.topics.retrieve) {
+    if (that.config.topics.retrieve.notify) {
+      if (that.config.topics.retrieve.get) {
+        for (i = 0; i < that.config.relayCount; i++) {
+          let topic = that.config.topics.retrieve.get.replace(/{id}/, i + 1);
+          that.mqttClient.subscribe(topic, {'qos': that.config.mqtt.qos || 1});
+          that.mqttGetOnTopics.push([topic, i]);
+        }
+      }
+      for (i = 0; i < that.config.relayCount; i++) {
+        let topic = that.config.topics.retrieve.notify.replace(/{id}/, i + 1);
+        that.mqttClient.publish(topic, that.config.topics.retrieve.message || '', {'qos': that.config.mqtt.qos || 1});
+      }
+    } else {
+      that.log("\x1b[31mYou need to specify the notify topic to retrieve the state!\x1b[0m");
+    }
+  }
+}
+
+PowerStrip.prototype.mqttError = function(err) {
+  that.log("\x1b[31mAn error has ocurred while connecting to mqtt broker: " + err + "\x1b[0m");
+  that.mqttErrorCount++;
+  if (that.mqttErrorCount >= that.maxMqttErrorCount) {
+    that.mqttClient.end();
+  }
+}
+
+PowerStrip.prototype.mqttMessage = function(topic, msg, packet) {
+  if (that.checkValueInArrayOfArrays(topic, that.mqttGetOnTopics)) {
+    let id = that.checkValueInArrayOfArrays(topic, that.mqttGetOnTopics)[1];
+    if (msg == (that.config.onValue || "true")) {
+      that.services[id].getCharacteristic(Characteristic.On).updateValue(true);
+    } else if (msg == (that.config.offValue || "false")) {
+      that.services[id].getCharacteristic(Characteristic.On).updateValue(false);
+    } else {
+      that.log("\x1b[31mThe received value on the getOn topic (" + msg + ") is not one of the accepted values, state cannot be set.\x1b[0m");
+    }
+  }
+}
+
+PowerStrip.prototype.setOn = function (id, on, callback) {
+  if (this.config.topics.setOn) {
+    let topic = this.config.topics.setOn.replace(/{id}/, id);
+    if (on) {
+      this.mqttClient.publish(topic, this.config.onValue || "true", {'qos': that.config.mqtt.qos || 1});
+    } else {
+      this.mqttClient.publish(topic, this.config.offValue || "false", {'qos': that.config.mqtt.qos || 1});
+    }
+    callback();
+  } else {
+    this.log("\x1b[31msetOn topic has not been specified so the state can't be set\x1b[0m");
+    callback(on);
+  }
+}
+
+PowerStrip.prototype.getServices = function() {
+  return this.services;
+};
+
+PowerStrip.prototype.checkValueInArrayOfArrays = function(value, array) {
+  let arrayToReturn = null;
+
+  array.forEach(i => {
+    i.forEach((j, n, arr) => {
+      if (j == value) {
+        arrayToReturn = arr;
+      }
+    });
+  });
+
+  return arrayToReturn;
+}
